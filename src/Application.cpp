43f31862d12c7191e10df81cb6fa5114b40d7b58
Joe <51638822+tso996@users.noreply.github.com>
2022-09-04 12:42:42 +0100

    glew set up

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,16 +1,16 @@
 #include <GL/glew.h>
-
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
     GLFWwindow* window;
 
-    glewInit();
     /* Initialize the library */
     if (!glfwInit())
         return -1;
 
+    
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
@@ -21,6 +21,14 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK) {
+        std::cout << "GLEW did not initialize properly!" << std::endl;
+    }
+    else {
+        std::cout << "GLEW initialized.." << std::endl;
+        std::cout << glGetString(GL_VERSION) << std::endl;
+    }
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
