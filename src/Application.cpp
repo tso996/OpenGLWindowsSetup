3090246e329274812d43fa3f90e61fd749801e2d
Joe <51638822+tso996@users.noreply.github.com>
2022-09-06 00:04:59 +0100

    indices

--- src/Application.cpp
+++ src/Application.cpp
@@ -142,19 +142,27 @@ int main(void)
     float positions[12] = {
           0.5f, 0.5f,
           0.5f, -0.5f,
-         -0.5f, -0.5f,
-         
-          0.5f, 0.5f,
+         -0.5f, -0.5f,      
          -0.5f, 0.5f,
-         -0.5f, -0.5f
+    };
+
+    unsigned int indices[] = {
+        0, 1, 2,
+        2, 3, 0
     };
 
     unsigned int buffer{};
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 2, positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 4 * 2, positions, GL_STATIC_DRAW);
+
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
     glEnableVertexAttribArray(0);
+
+    unsigned int indexBuffer{};
+    glGenBuffers(1, &indexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(float) * 6, indices, GL_STATIC_DRAW);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
@@ -190,7 +198,7 @@ int main(void)
         glClear(GL_COLOR_BUFFER_BIT);
 
 
-        glDrawArrays(GL_TRIANGLES, 0,6);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
 
         glEnd();
