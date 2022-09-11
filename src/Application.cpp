5d2cded8b69439c33f273c7067ce79f7210e1031
Joe <51638822+tso996@users.noreply.github.com>
2022-09-11 22:12:13 +0100

    shader finished

--- src/Application.cpp
+++ src/Application.cpp
@@ -4,6 +4,21 @@
 #include <GLFW/glfw3.h>
 
 
+const char* vertexShaderSource = "#version 330 core\n"
+"layout(location=0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main(){\n"
+"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\0";
+
+
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	std::cout << "framebuffer_size_callback function called!" << std::endl;
@@ -15,6 +30,33 @@ void processInput(GLFWwindow* window) {
 		glfwSetWindowShouldClose(window, true);
 	}
 }
+
+void checkShaderCompilation(unsigned int objectID) {
+	int s;
+	char infoLog[512];
+	glGetShaderiv(objectID, GL_COMPILE_STATUS, &s);
+
+	if (!s) {
+		glGetShaderInfoLog(objectID, 512, NULL, infoLog);
+		std::cout << "shader compilation error: " << infoLog << std::endl;
+	}
+	else {
+		std::cout << "shader compiled ok" << std::endl;
+	}
+}
+void checkProgramCompilation(unsigned int objectID) {
+	int s;
+	char infoLog[512];
+	glGetProgramiv(objectID, GL_LINK_STATUS, &s);
+	if (!s) {
+		glGetProgramInfoLog(objectID, 512, NULL, infoLog);
+		std::cout << "program compilation error: " << infoLog << std::endl;
+	}
+	else {
+		std::cout << "program compiled ok" << std::endl;
+	}
+}
+
 
 int main(int args, char* argv[]) {
 	glfwInit();
@@ -62,14 +104,39 @@ int main(int args, char* argv[]) {
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	//making and compiling VERTEX SHADER object
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	checkShaderCompilation(vertexShader);
 
+	//compiling FRAGMENT SHADER
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	checkShaderCompilation(fragmentShader);
+
+	//shader program object so that linking v and f shader are possible
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	checkProgramCompilation(shaderProgram);//only for shaders
+
+	glUseProgram(shaderProgram);
+
+	//free gpu memory after linking
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
 		//rendering commands
 		glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-
 
 
 		//check for events and swap buffers
