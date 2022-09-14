e3de5a3bd63e85727d1299e3747d196f39711903
Joe <51638822+tso996@users.noreply.github.com>
2022-09-14 21:40:19 +0100

    controling fragment variable through vertex shader

--- src/Application.cpp
+++ src/Application.cpp
@@ -26,15 +26,20 @@ const unsigned int SCR_HEIGHT = 600;
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"out vec4 vertexColor;"
 "void main()\n"
 "{\n"
 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"   vertexColor = vec4(0.5, 1.0, 0.0, 1.0);\n"
 "}\0";
+
+//output from fragment shader is pixel
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec4 vertexColor;"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"   FragColor = vertexColor;\n"
 "}\n\0";
 
 int main()
